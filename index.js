a56cd031f4a9f9577cac3ad1b79fbf863eb27113
vladch1 <mail.vchpro@yandex.ru>
2023-11-30 13:06:14 +0300

    Add inline_query handling
    Add main logic (inline_query handling and answering)

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ const basicMath = require('advanced-calculator');
 const bot = new Telegraf(config.BOT_TOKEN);
 
 
+bot.on('inline_query', async ctx => {
+    const query = ctx.update.inline_query.query;
+    try {
+        const result = basicMath.evaluate(query);
+        await ctx.answerInlineQuery([{
+            type: "article",
+            id: 'result',
+            title: result,
+            description: query,
+            input_message_content: {
+                message_text: `${query}=\n<b>${result}</b>`,
+                parse_mode: "HTML"
+            },
+        }]);
+    } catch (e) {
+        await ctx.answerInlineQuery([{
+            type: "article",
+            id: 'result',
+            title: language.enterExpression,
+            input_message_content: {
+                message_text: language.enterExpression,
+            },
+        }]);
+    }
+});
+
 bot.launch()
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
